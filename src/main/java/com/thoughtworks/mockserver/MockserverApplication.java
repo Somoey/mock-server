e6dc8bc1b2ba0f75166b8700f01c061325a29279
mengyang <mengyang.sun@thoughtworks.com>
2020-08-31 13:04:51 +0800

    mock demo

--- src/main/java/com/thoughtworks/mockserver/MockserverApplication.java
+++ src/main/java/com/thoughtworks/mockserver/MockserverApplication.java
@@ -1,13 +1,25 @@
 package com.thoughtworks.mockserver;
 
-import org.springframework.boot.SpringApplication;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
 @SpringBootApplication
 public class MockserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MockserverApplication.class, args);
-	}
+	private static final int PORT=8081;//定义mock端口
+	private static final String LOCALHOST="localhost";//定义mock服务地址
+	private static final String MOCK_DIR="mock";//定义mapping文件目录
 
+	public static void main(String[] args) {
+		final WireMockConfiguration config=wireMockConfig()
+				.port(PORT)
+				.usingFilesUnderClasspath(MOCK_DIR);
+		final WireMockServer WireMockServer = new WireMockServer(config);
+		WireMockServer.start();
+		WireMock.configureFor(LOCALHOST,PORT);
+	}
 }
