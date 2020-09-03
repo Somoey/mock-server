928e83539b740a7eed3c76631fcafdfd9f16c4f1
mengyang <mengyang.sun@thoughtworks.com>
2020-09-03 15:54:09 +0800

    refactor: modify delete mapping response status to 204

--- src/main/java/com/thoughtworks/mockserver/controller/MockController.java
+++ src/main/java/com/thoughtworks/mockserver/controller/MockController.java
@@ -1,6 +1,5 @@
 package com.thoughtworks.mockserver.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thoughtworks.mockserver.entity.BuildingInfo;
 import com.thoughtworks.mockserver.entity.HouseNumberInfo;
 import com.thoughtworks.mockserver.entity.PhaseInfo;
@@ -15,13 +14,14 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -67,6 +67,7 @@ public class MockController {
         return DataGenerator.ListHouseNumber();
     }
 
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @DeleteMapping("users/houses")
     public Result deleteHouseData(@RequestBody DeleteHouseDataRequest request) {
         return new Result("ok");
